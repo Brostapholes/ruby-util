d2dc2ef26646c9daf8656bace94aba2a6e78a108
Todd Downey <todd.w.downey@cerner.com>
2018-06-04 19:31:41 -0500

    Update Logcleaner Script
    Updating logcleaner script and gitignore.

--- logcleaner.rb
+++ logcleaner.rb
@@ -1,17 +1,31 @@
 #!/usr/bin/env ruby
-require 'fileutils'
 
 dirs = []
+safiles = []
 
-FileUtils.cd('/var/log/') do
-  dirs.push(FileUtils.pwd)
-  dirs.push(Dir.glob('/var/log/**/'))
-  dirs.delete('/var/log')
+# Create array of /var/log subdirectories
+Dir.glob('/var/log/**/').each do |x|
+  dirs.push(x)
 end
 
+# Clean up files in the /var/log directory and subdirectories
 dirs.each do |dir|
-  FileUtils.cd(dir) do
-    FileUtils.rm Dir.glob('*.gz'), force: true, verbose: true
-    FileUtils.rm Dir.glob('*201*'), force: true, verbose: true
+  Dir.chdir(dir) do
+    Dir.glob('*.gz').each { |file| File.delete(file) }
+    Dir.glob('*.old').each { |file| File.delete(file) }
+    Dir.glob('*201*').each { |file| File.delete(file) }
+  end
+end
+
+# Array of files in /var/log/sa
+Dir.glob('/var/log/sa/*').each do |x|
+  safiles.push(x)
+end
+
+# SA Files cleanup
+Dir.chdir('/var/log/sa') do
+  safiles.each do |x|
+    `gzip --best #{x}`
+    File.delete(x)
   end
 end
