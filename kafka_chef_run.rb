def8ead83b632beadc3c05260f80a1e3cd6d0ad3
Todd Downey <toddd79@gmail.com>
2020-04-18 23:06:06 -0500

    Updare

--- kafka_chef_run.rb
+++ kafka_chef_run.rb
@@ -1,6 +1,13 @@
-# !/usr/bin/env ruby
+#!/bin/env ruby
 
 require 'fileutils'
+
+#Stoo Kafka Process
+
+# Clear Kafka logs
+Dir['/var/db/kafka/*'].each do |x|
+	FileUtils.rm_rf(x, verbose: true)
+end
 
 # Clear Chef cache
 Dir['/var/chef/cache/*'].each do |x|
@@ -9,6 +16,3 @@ end
 
 # Remove they ruby-build file
 FileUtils.rm_f('/usr/local/bin/ruby-build', verbose: true)
-
-# Run chef-client
-system('chef-client')
--- logcleaner.rb
+++ logcleaner.rb
@@ -7,29 +7,29 @@ safiles = []
 
 # Create array of /var/log subdirectories
 Dir.glob('/var/log/**/').each do |x|
-  dirs.push(x)
+	dirs.push(x)
 end
 
 # Clean up files in the /var/log directory and subdirectories
 dirs.each do |dir|
-  Dir.chdir(dir) do
-    Dir.glob('*.gz').each { |file| File.delete(file) }
-    Dir.glob('*.old').each { |file| File.delete(file) }
-    Dir.glob('*201*').each { |file| File.delete(file) }
-  end
+	Dir.chdir(dir) do
+		Dir.glob('*.gz').each {|file| File.delete(file)}
+		Dir.glob('*.old').each {|file| File.delete(file)}
+		Dir.glob('*201*').each {|file| File.delete(file)}
+	end
 end
 # Array of files in /var/log/sa
 Dir.glob('/var/log/sa/*').each do |x|
-  safiles.push(x)
+	safiles.push(x)
 end
 
 # SA Files cleanup
 Dir.chdir('/var/log/sa') do
-  safiles.each do |y|
-    gz = "#{y}.gz"
-    Zlib::GzipWriter.open(gz) do |z|
-      z.write File.read(y)
-    end
-    File.delete(y)
-  end
+	safiles.each do |y|
+		gz = "#{y}.gz"
+		Zlib::GzipWriter.open(gz) do |z|
+			z.write File.read(y)
+		end
+		File.delete(y)
+	end
 end
